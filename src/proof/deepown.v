d45b6ac10a79b4bee1f2e6fafc0dfc55963445bc
Xudong Sun <xudongs3@illinois.edu>
2025-12-08 20:25:47 -0600

    Make deepown read only

--- src/proof/deepown.v
+++ src/proof/deepown.v
@@ -9,21 +9,23 @@ Section proof.
 Context `{hG: !heapGS Σ} {go_ctx: GoContext}.
 
 Axiom deepown_Time: time.Time.t → iProp Σ.
+Global Instance deepown_Time_persistent v : Persistent (deepown_Time v).
+Proof. Admitted.
 
 Definition deepown_Time_loc l: iProp Σ :=
-  ⌜ l = null ⌝ ∨ ∃ (v: time.Time.t), l ↦ v ∗ deepown_Time v.
+  ⌜ l = null ⌝ ∨ ∃ (v: time.Time.t), l ↦□ v ∗ deepown_Time v.
 
 Definition deepown_w64_loc l : iProp Σ :=
-  ⌜ l = null ⌝ ∨ ∃ (v: w64), l ↦ v.
+  ⌜ l = null ⌝ ∨ ∃ (v: w64), l ↦□ v.
 
 Definition deepown_bool_loc l : iProp Σ :=
-  ⌜ l = null ⌝ ∨ ∃ (v: bool), l ↦ v.
+  ⌜ l = null ⌝ ∨ ∃ (v: bool), l ↦□ v.
 
 Definition deepown_string_slice l : iProp Σ :=
-  ⌜ l = slice.nil ⌝ ∨ ∃ (vs: list go_string), l ↦* vs.
+  ⌜ l = slice.nil ⌝ ∨ ∃ (vs: list go_string), l ↦*□ vs.
 
 Definition deepown_string_string_map l : iProp Σ :=
-  ⌜ l = null ⌝ ∨ ∃ (v: gmap go_string go_string), l ↦$ v.
+  ⌜ l = null ⌝ ∨ ∃ (v: gmap go_string go_string), l ↦$□ v.
 
 Definition deepown_OwnerReference (v: v1.OwnerReference.t) : iProp Σ :=
   deepown_bool_loc v.(v1.OwnerReference.Controller') ∗
@@ -31,13 +33,15 @@ Definition deepown_OwnerReference (v: v1.OwnerReference.t) : iProp Σ :=
 
 Definition deepown_OwnerReference_slice l : iProp Σ :=
   ⌜ l = slice.nil ⌝ ∨
-  ∃ (vs : list v1.OwnerReference.t), l ↦* vs ∗ [∗ list] v ∈ vs, deepown_OwnerReference v.
+  ∃ (vs : list v1.OwnerReference.t), l ↦*□ vs ∗ [∗ list] v ∈ vs, deepown_OwnerReference v.
 
 Axiom deepown_ManagedFieldsEntry: v1.ManagedFieldsEntry.t → iProp Σ.
+Global Instance deepown_ManagedFieldsEntry_persistent v : Persistent (deepown_ManagedFieldsEntry v).
+Proof. Admitted.
 
 Definition deepown_ManagedFieldsEntry_slice l : iProp Σ :=
   ⌜ l = slice.nil ⌝ ∨
-  ∃ (vs : list v1.ManagedFieldsEntry.t), l ↦* vs ∗ [∗ list] v ∈ vs, deepown_ManagedFieldsEntry v.
+  ∃ (vs : list v1.ManagedFieldsEntry.t), l ↦*□ vs ∗ [∗ list] v ∈ vs, deepown_ManagedFieldsEntry v.
 
 Definition deepown_ObjectMeta (v: v1.ObjectMeta.t) : iProp Σ :=
   deepown_Time_loc v.(v1.ObjectMeta.DeletionTimestamp') ∗
@@ -49,10 +53,14 @@ Definition deepown_ObjectMeta (v: v1.ObjectMeta.t) : iProp Σ :=
   deepown_ManagedFieldsEntry_slice v.(v1.ObjectMeta.ManagedFields').
 
 Axiom deepown_PodSpec: v1.PodSpec.t → iProp Σ.
+Global Instance deepown_PodSpec_persistent v : Persistent (deepown_PodSpec v).
+Proof. Admitted.
 
 Axiom deepown_PodStatus: v1.PodStatus.t → iProp Σ.
+Global Instance deepown_PodStatus_persistent v : Persistent (deepown_PodStatus v).
+Proof. Admitted.
 
-Definition deepcopy_Pod (v: v1.Pod.t) : iProp Σ :=
+Definition deepown_Pod (v: v1.Pod.t) : iProp Σ :=
   deepown_ObjectMeta v.(v1.Pod.ObjectMeta') ∗
   deepown_PodSpec v.(v1.Pod.Spec') ∗
   deepown_PodStatus v.(v1.Pod.Status').
