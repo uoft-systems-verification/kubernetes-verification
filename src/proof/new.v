0aa83346db5a30a88d2504c86f349685bb43e5be
Xudong Sun <xudongs3@illinois.edu>
2026-02-07 19:59:33 -0600

    Introduce own_tombstone_p_frag

--- src/proof/new.v
+++ src/proof/new.v
@@ -10,7 +10,7 @@ From New.proof.big_op Require Export big_sepL big_sepM.
 Section spec.
 Context `{hG: !heapGS Σ} {go_ctx: GoContext}.
 
-Axiom own_meta_frag: gname → KKey.t → types.UID.t → option ObjectMetaV.t → iProp Σ.
+Axiom own_meta_frag: gname → KKey.t → types.UID.t → ObjectMetaV.t → iProp Σ.
 
 Axiom own_weak_spec_frag: gname → KKey.t → types.UID.t → ObjectSpecV.t → iProp Σ.
 
@@ -20,11 +20,14 @@ Axiom own_weak_children_frag: gname → KKey.t → types.UID.t → gset KKey.t �
 
 Axiom own_fresh_key_frag: gname → KKey.t → iProp Σ.
 
-(* A snapshot of the object given the key, uid, and rv. It's persistent. *)
-Axiom own_snapshot_ro_frag: gname → KKey.t → types.UID.t → go_string → KObjectV.t → iProp Σ.
+(* A tombstone of the object and its uid. It's persistent. *)
+Axiom own_tombstone_p_frag: gname → KKey.t → types.UID.t → iProp Σ.
 
-Definition own_snapshot_ro_frag_of γ kobj: iProp Σ :=
-  own_snapshot_ro_frag γ
+(* A snapshot of the object given the key, uid, and rv. It's persistent. *)
+Axiom own_snapshot_p_frag: gname → KKey.t → types.UID.t → go_string → KObjectV.t → iProp Σ.
+
+Definition own_snapshot_p_frag_of γ kobj: iProp Σ :=
+  own_snapshot_p_frag γ
     (KObjectV.key kobj)
     (KObjectV.objectmeta kobj).(ObjectMetaV.UID')
     (KObjectV.objectmeta kobj).(ObjectMetaV.ResourceVersion')
@@ -42,7 +45,7 @@ Lemma wp_State__get γ l key:
         ⌜ KObjectV.valid kobj ⌝ ∗
         ⌜ key = KObjectV.key kobj ⌝ ∗
         KObjectV.deepown_i i kobj 1 ∗
-        own_snapshot_ro_frag_of γ kobj
+        own_snapshot_p_frag_of γ kobj
       ) ∨
       ⌜ err ≠ interface.nil ⌝
   }}}.
@@ -51,7 +54,7 @@ Proof. Admitted.
 Lemma wp_State__get_none γ l key uid:
   {{{ is_pkg_init apimodel ∗
       "#Hisk" ∷ is_kubernetes γ l ∗
-      "Hmeta" ∷ own_meta_frag γ key uid None
+      "#Htomb" ∷ own_tombstone_p_frag γ key uid
   }}}
     l @ (ptrT.id apimodel.State.id) @ "get" #key
   {{{ i (err: error.t) kobj, RET (#i, #err);
@@ -60,11 +63,11 @@ Lemma wp_State__get_none γ l key uid:
           ⌜ key = KObjectV.key kobj ⌝ ∗
           ⌜ uid ≠ (KObjectV.objectmeta kobj).(ObjectMetaV.UID') ⌝ ∗
           KObjectV.deepown_i i kobj 1 ∗
-          own_snapshot_ro_frag_of γ kobj
+          own_snapshot_p_frag_of γ kobj
         ) ∨
         ⌜ err ≠ interface.nil ⌝
       )
-      ∗ own_meta_frag γ key uid None 
+      ∗ own_tombstone_p_frag γ key uid 
   }}}.
 Proof. Admitted.
 
@@ -72,7 +75,7 @@ Lemma wp_State__get_some γ l key uid kmeta kspec_o kstatus_o:
   {{{ is_pkg_init apimodel ∗
       "#Hisk" ∷ is_kubernetes γ l ∗
       "%Huid" ∷ ⌜ uid = kmeta.(ObjectMetaV.UID') ⌝ ∗
-      "Hmeta" ∷ own_meta_frag γ key uid (Some kmeta) ∗
+      "Hmeta" ∷ own_meta_frag γ key uid kmeta ∗
       "Hspec" ∷ (match kspec_o with
                   | Some kspec => own_weak_spec_frag γ key uid kspec
                   | None => True
@@ -88,7 +91,7 @@ Lemma wp_State__get_some γ l key uid kmeta kspec_o kstatus_o:
       ⌜ key = KObjectV.key kobj ⌝ ∗
       ⌜ kmeta = KObjectV.objectmeta kobj ⌝ ∗
       KObjectV.deepown_i i kobj 1 ∗
-      own_meta_frag γ key uid (Some kmeta) ∗
+      own_meta_frag γ key uid kmeta ∗
       (match kspec_o with
       | Some kspec => own_weak_spec_frag γ key uid kspec ∗ ⌜ kspec = KObjectV.spec kobj ⌝
       | None => True
@@ -97,7 +100,7 @@ Lemma wp_State__get_some γ l key uid kmeta kspec_o kstatus_o:
       | Some kstatus => own_weak_status_frag γ key uid kstatus ∗ ⌜ kstatus = KObjectV.status kobj ⌝
       | None => True
       end) ∗
-      own_snapshot_ro_frag_of γ kobj
+      own_snapshot_p_frag_of γ kobj
   }}}.
 Proof. Admitted.
 
@@ -111,7 +114,7 @@ Lemma wp_State__create γ l kind namespace i kobj key parent_key parent_uid pare
       "%Hkey_eq" ∷ ⌜ key = (KObjectV.key kobj) ⌝ ∗
       "Hdeepown_i" ∷ KObjectV.deepown_i i kobj 1 ∗
       "Hfresh_key" ∷ own_fresh_key_frag γ key ∗
-      "Hparent_meta" ∷ own_meta_frag γ parent_key parent_uid (Some parent_meta) ∗
+      "Hparent_meta" ∷ own_meta_frag γ parent_key parent_uid parent_meta ∗
       "Hchildren" ∷ own_weak_children_frag γ parent_key parent_uid children
   }}}
     l @ (ptrT.id apimodel.State.id) @ "create" #kind #namespace #i
@@ -124,13 +127,13 @@ Lemma wp_State__create γ l kind namespace i kobj key parent_key parent_uid pare
       ⌜ key ∉ children ⌝ ∗
       ⌜ uid = (KObjectV.objectmeta kobj').(ObjectMetaV.UID') ⌝ ∗
       KObjectV.deepown_i i' kobj' 1 ∗
-      own_meta_frag γ key uid (Some (KObjectV.objectmeta kobj')) ∗
+      own_meta_frag γ key uid (KObjectV.objectmeta kobj') ∗
       own_weak_spec_frag γ key uid (KObjectV.spec kobj') ∗
       own_weak_status_frag γ key uid (KObjectV.status kobj') ∗
-      own_meta_frag γ parent_key parent_uid (Some parent_meta) ∗
+      own_meta_frag γ parent_key parent_uid parent_meta ∗
       own_weak_children_frag γ parent_key parent_uid (children ∪ {[key]}) ∗
       own_weak_children_frag γ key uid ∅ ∗
-      own_snapshot_ro_frag_of γ kobj'
+      own_snapshot_p_frag_of γ kobj'
   }}}.
 Proof. Admitted.
 
@@ -142,7 +145,7 @@ Lemma wp_State__nameless_create γ l kind namespace i kobj parent_key parent_uid
       "%Hparent_of" ∷ ⌜ obj_has_controller_parent_of kobj parent_key.(KKey.Kind') parent_key.(KKey.Name') parent_uid ⌝ ∗
       "%Hwf" ∷ ⌜ KObjectV.valid_nameless_create namespace kobj ⌝ ∗
       "Hdeepown_i" ∷ KObjectV.deepown_i i kobj 1 ∗
-      "Hparent_meta" ∷ own_meta_frag γ parent_key parent_uid (Some parent_meta) ∗
+      "Hparent_meta" ∷ own_meta_frag γ parent_key parent_uid parent_meta ∗
       "Hchildren" ∷ own_weak_children_frag γ parent_key parent_uid children
   }}}
     l @ (ptrT.id apimodel.State.id) @ "create" #kind #namespace #i
@@ -155,13 +158,13 @@ Lemma wp_State__nameless_create γ l kind namespace i kobj parent_key parent_uid
       ⌜ key ∉ children ⌝ ∗
       ⌜ uid = (KObjectV.objectmeta kobj').(ObjectMetaV.UID') ⌝ ∗
       KObjectV.deepown_i i' kobj' 1 ∗
-      own_meta_frag γ key uid (Some (KObjectV.objectmeta kobj')) ∗
+      own_meta_frag γ key uid (KObjectV.objectmeta kobj') ∗
       own_weak_spec_frag γ key uid (KObjectV.spec kobj') ∗
       own_weak_status_frag γ key uid (KObjectV.status kobj') ∗
-      own_meta_frag γ parent_key parent_uid (Some parent_meta) ∗
+      own_meta_frag γ parent_key parent_uid parent_meta ∗
       own_weak_children_frag γ parent_key parent_uid (children ∪ {[key]}) ∗
       own_weak_children_frag γ key uid ∅ ∗
-      own_snapshot_ro_frag_of γ kobj'
+      own_snapshot_p_frag_of γ kobj'
   }}}.
 Proof. Admitted.
 
@@ -169,16 +172,18 @@ Lemma wp_State__delete γ l key uid kmeta parent_key parent_uid children:
   {{{ is_pkg_init apimodel ∗
       "#Hisk" ∷ is_kubernetes γ l ∗
       "%His_child" ∷ ⌜ key ∈ children ⌝ ∗
-      "Hmeta" ∷ own_meta_frag γ key uid (Some kmeta) ∗
+      "Hmeta" ∷ own_meta_frag γ key uid kmeta ∗
       "Hchildren" ∷ own_weak_children_frag γ parent_key parent_uid children
   }}}
     l @ (ptrT.id apimodel.State.id) @ "delete" #key #null #null
   {{{ (kmeta': ObjectMetaV.t), RET #interface.nil;
       (( ⌜ ObjectMetaV.deleting kmeta kmeta' ⌝ ∗
-          own_meta_frag γ key uid (Some kmeta') ∗
+          own_meta_frag γ key uid kmeta' ∗
           own_weak_children_frag γ parent_key parent_uid children
-        ) ∨
-        own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+        ) ∨ (
+          own_tombstone_p_frag γ key uid ∗
+          own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+        )
       )
   }}}.
 Proof. Admitted.
@@ -188,7 +193,7 @@ Lemma wp_State__delete_uid γ l key uid_ptr uid current_uid kmeta parent_key par
       "#Hisk" ∷ is_kubernetes γ l ∗
       "%His_child" ∷ ⌜ key ∈ children ⌝ ∗
       "Huid_ptr" ∷ uid_ptr ↦ uid ∗
-      "Hmeta" ∷ own_meta_frag γ key current_uid (Some kmeta) ∗
+      "Hmeta" ∷ own_meta_frag γ key current_uid kmeta ∗
       "Hchildren" ∷ own_weak_children_frag γ parent_key parent_uid children
   }}}
     l @ (ptrT.id apimodel.State.id) @ "delete" #key #uid_ptr #null
@@ -196,10 +201,12 @@ Lemma wp_State__delete_uid γ l key uid_ptr uid current_uid kmeta parent_key par
       ( ⌜ uid = current_uid ⌝ ∗
         ⌜ err = interface.nil ⌝ ∗
         ((⌜ ObjectMetaV.deleting kmeta kmeta' ⌝ ∗
-            own_meta_frag γ key current_uid (Some kmeta') ∗
+            own_meta_frag γ key current_uid kmeta' ∗
             own_weak_children_frag γ parent_key parent_uid children
-          ) ∨
-          own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+          ) ∨ (
+            own_tombstone_p_frag γ key uid ∗
+            own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+          )
         )
       ) ∨
       ( ⌜ uid ≠ current_uid ⌝ ∗
@@ -211,11 +218,11 @@ Proof. Admitted.
 Lemma wp_State__delete_rv γ l key uid_ptr uid rv_ptr rv prev_kobj current_uid kmeta parent_key parent_uid children:
   {{{ is_pkg_init apimodel ∗
       "#Hisk" ∷ is_kubernetes γ l ∗
-      "#Hsnapshot" ∷ own_snapshot_ro_frag γ key uid rv prev_kobj ∗
+      "#Hsnapshot" ∷ own_snapshot_p_frag γ key uid rv prev_kobj ∗
       "%His_child" ∷ ⌜ key ∈ children ⌝ ∗
       "Huid_ptr" ∷ uid_ptr ↦ uid ∗
       "Hrv_ptr" ∷ rv_ptr ↦ rv ∗
-      "Hmeta" ∷ own_meta_frag γ key current_uid (Some kmeta) ∗
+      "Hmeta" ∷ own_meta_frag γ key current_uid kmeta ∗
       "Hchildren" ∷ own_weak_children_frag γ parent_key parent_uid children
   }}}
     l @ (ptrT.id apimodel.State.id) @ "delete" #key #uid_ptr #rv_ptr
@@ -224,10 +231,12 @@ Lemma wp_State__delete_rv γ l key uid_ptr uid rv_ptr rv prev_kobj current_uid k
         ⌜ rv = kmeta.(ObjectMetaV.ResourceVersion') ⌝ ∗
         ⌜ err = interface.nil ⌝ ∗
         ((⌜ ObjectMetaV.deleting kmeta kmeta' ⌝ ∗
-            own_meta_frag γ key current_uid (Some kmeta') ∗
+            own_meta_frag γ key current_uid kmeta' ∗
             own_weak_children_frag γ parent_key parent_uid children
-          ) ∨
-          own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+          ) ∨ (
+            own_tombstone_p_frag γ key uid ∗
+            own_weak_children_frag γ parent_key parent_uid (children ∖ {[key]})
+          )
         )
       ) ∨
       ( (⌜ uid ≠ current_uid ⌝ ∨ ⌜ rv ≠ kmeta.(ObjectMetaV.ResourceVersion') ⌝) ∗
@@ -239,14 +248,14 @@ Proof. Admitted.
 Lemma wp_State__update γ l kind namespace i key uid rv prev_kobj kobj kmeta kspec:
   {{{ is_pkg_init apimodel ∗
       "#Hisk" ∷ is_kubernetes γ l ∗
-      "#Hsnapshot" ∷ own_snapshot_ro_frag γ key uid rv prev_kobj ∗
+      "#Hsnapshot" ∷ own_snapshot_p_frag γ key uid rv prev_kobj ∗
       "%Hkind_eq" ∷ ⌜ kind = KObjectV.kind kobj ⌝ ∗
       "%Hkey_eq" ∷ ⌜ key = KObjectV.key kobj ⌝ ∗
       "%Hv" ∷ ⌜ KObjectV.valid kobj ⌝ ∗
       "%Hvu" ∷ ⌜ KObjectV.valid_update namespace prev_kobj kobj ⌝ ∗
       "%Hsu" ∷ ⌜ ObjectMetaV.simple_update (KObjectV.objectmeta prev_kobj) (KObjectV.objectmeta kobj) ⌝ ∗
       "Hdeepown_i" ∷ KObjectV.deepown_i i kobj 1 ∗
-      "Hmeta" ∷ own_meta_frag γ key uid (Some kmeta) ∗
+      "Hmeta" ∷ own_meta_frag γ key uid kmeta ∗
       "Hspec" ∷ own_weak_spec_frag γ key uid kspec
   }}}
     l @ (ptrT.id apimodel.State.id) @ "update" #kind #namespace #i
@@ -257,13 +266,13 @@ Lemma wp_State__update γ l kind namespace i key uid rv prev_kobj kobj kmeta ksp
         ⌜ ObjectMetaV.updated kmeta (KObjectV.objectmeta kobj) (KObjectV.objectmeta kobj') ⌝ ∗
         ⌜ ObjectSpecV.updated kspec (KObjectV.spec kobj) (KObjectV.spec kobj') ⌝ ∗
         KObjectV.deepown_i i' kobj' 1 ∗
-        own_meta_frag γ key uid (Some (KObjectV.objectmeta kobj')) ∗
+        own_meta_frag γ key uid (KObjectV.objectmeta kobj') ∗
         own_weak_spec_frag γ key uid (KObjectV.spec kobj') ∗
-        own_snapshot_ro_frag_of γ kobj'
+        own_snapshot_p_frag_of γ kobj'
       ) ∨ (
         ⌜ kmeta.(ObjectMetaV.ResourceVersion') ≠ rv ⌝ ∗
         ⌜ err ≠ interface.nil ⌝ ∗
-        own_meta_frag γ key uid (Some kmeta) ∗
+        own_meta_frag γ key uid kmeta ∗
         own_weak_spec_frag γ key uid kspec
       )
   }}}.
