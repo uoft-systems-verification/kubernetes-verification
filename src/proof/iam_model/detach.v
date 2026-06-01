2b23df7b7e3dd10e5672670b6f3ef1b5e907551a
Xudong Sun <xudongs3@illinois.edu>
2026-06-01 10:59:27 -0400

    Simplify detach spec

--- src/proof/iam_model/detach.v
+++ src/proof/iam_model/detach.v
@@ -10,10 +10,10 @@ Local Set Default Proof Using "All".
 Lemma wp_State__DetachIdentityPolicy
     γ l identity policy_ids policy_id policy resource attachments q :
   {{{ "#Hiam" ∷ is_iam γ l ∗
-      "%Hattached" ∷ ⌜ policy_ids !! policy_id = Some tt ⌝ ∗
       "Hidentity" ∷ own_iam_identity_frag γ identity 1 policy_ids ∗
       "Hpolicy" ∷ own_iam_policy_frag γ policy_id q policy ∗
       "Hattachments" ∷ own_iam_attachments_frag γ resource 1 attachments ∗
+      "%Hattached" ∷ ⌜ policy_ids !! policy_id = Some tt ⌝ ∗
       "%Hresource_eq" ∷ ⌜ resource = policy.(iammodel.IdentityPolicy.Resource') ⌝
   }}}
     l @! (go.PointerType iammodel.State) @! "DetachIdentityPolicy" #identity #policy_id
@@ -21,15 +21,12 @@ Lemma wp_State__DetachIdentityPolicy
       "Hidentity" ∷ own_iam_identity_frag γ identity 1 (delete policy_id policy_ids) ∗
       "Hpolicy" ∷ own_iam_policy_frag γ policy_id q policy ∗
       "Hattachments" ∷ own_iam_attachments_frag γ resource 1 attachments' ∗
-      "%Hattachments_other" ∷ ⌜ ∀ identity',
-        identity' ≠ identity →
-        attachments' !! identity' = attachments !! identity' ⌝ ∗
-      "%Hattachments_identity" ∷ ⌜
+      "%Hattachments" ∷ ⌜
         (attachments !! identity = Some 1%nat ∧
-          attachments' !! identity = None) ∨
+          attachments' = delete identity attachments) ∨
         ∃ n, attachments !! identity = Some n ∧
           (2 ≤ n)%nat ∧
-          attachments' !! identity = Some (n - 1)%nat ⌝
+          attachments' = <[identity := (n - 1)%nat]> attachments ⌝
   }}}.
 Proof. Admitted.
 
