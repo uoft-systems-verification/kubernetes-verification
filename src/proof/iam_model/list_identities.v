01b98074b5446b6dbd2991a7eef3b046f6467c43
Xudong Sun <xudongs3@illinois.edu>
2026-06-01 20:23:37 -0400

    Inline helper lemma

--- src/proof/iam_model/list_identities.v
+++ src/proof/iam_model/list_identities.v
@@ -170,104 +170,6 @@ Proof.
       * apply IH; done.
 Qed.
 
-Definition for_map_postcondition_no_break P bv : iProp Σ :=
-  (⌜ bv = continue_val ⌝ ∗ P) ∨
-  (⌜ bv = execute_val ⌝ ∗ P).
-
-Lemma wp_map_for_range_return
-  `{!ZeroVal K} `{!EqDecision K} `{!Countable K} `{!ZeroVal V}
-  `{!go.IntoValInj K} `{!TypedPointsto K} `{!IntoValTyped K key_type} :
-  ∀ (P : list K → Z → iProp Σ) stk E (body : func.t) elem_type mref
-    (m : gmap K V) dq,
-  ∀ Φ,
-  mref ↦${dq} m -∗
-  (∀ keys,
-     ⌜ list_to_set keys = dom m ∧ length keys = size m ∧ NoDup keys ⌝ -∗
-     (P keys 0 ∗
-      □ (∀ i key v, ⌜ keys !! (Z.to_nat i) = Some key ∧ m !! key = Some v ⌝ -∗
-          P keys i -∗
-          WP #body #key #v @ stk; E
-            {{ bv, for_map_postcondition_no_break (P keys (i + 1)%Z) bv }}) ∗
-      (mref ↦${dq} m -∗ P keys (Z.of_nat (size m)) -∗ Φ execute_val))
-  ) -∗
-  WP map.for_range key_type elem_type #mref #body @ stk; E {{ Φ }}.
-Proof.
-  iIntros (P stk E body elem_type mref m dq Φ) "Hm HΦ".
-  wp_call.
-  iDestruct (own_map_not_nil with "[$]") as %?.
-  wp_if_destruct; first by exfalso.
-  rewrite own_map_unseal. iNamed "Hm".
-  wp_apply (_internal_wp_untyped_start_read with "Hown") as "Hown".
-  wp_apply (wp_InternalMapForRange with "[//]"). iIntros "%ks %Hdom'".
-  eapply go.is_map_domain_pure in Hdom'; last done.
-  destruct Hdom' as [Hks_nodup Hks].
-  assert (Forall (λ kv, ∃ (k : K), kv = #k) ks) as Heq.
-  { rewrite Forall_forall. intros kv Hk.
-    specialize (Hks kv). specialize (Hdom kv).
-    rewrite -Hks in Hk. apply Hdom in Hk. done. }
-  apply Forall_exists_Forall2_l in Heq as [keys Heq].
-  apply Forall2_fmap_2 in Heq. rewrite -list_eq_Forall2 in Heq.
-  rewrite list_fmap_id in Heq. subst ks.
-  iAssert (⌜ _ ⌝)%I with "[]" as "%Hfacts".
-  2: iDestruct ("HΦ" $! keys with "[]") as "(HP & #Hiter & HΦ)".
-  2: iPureIntro; exact Hfacts.
-  { iPureIntro. eassert _ as Hl.
-    2:{ split; first eexact Hl.
-        apply NoDup_fmap in Hks_nodup; last tc_solve.
-        rewrite <- (size_list_to_set (C:=gset K)); last done.
-        rewrite Hl. rewrite size_dom //. }
-    rewrite sets.set_eq. intros k.
-    rewrite elem_of_list_to_set.
-    specialize (Hks #k). specialize (Hagree k).
-    rewrite list_elem_of_fmap_inj in Hks.
-    rewrite -Hks. rewrite Hagree. rewrite elem_of_dom.
-    by destruct lookup. }
-  pose (i := 0%Z : Z).
-  change (P keys 0) with (P keys i).
-  iFreeze "Hiter".
-  replace (into_val <$> keys) with (into_val <$> (drop (Z.to_nat i) keys)).
-  2:{ rewrite drop_0 //. }
-  iThaw "Hiter".
-  iAssert (⌜ (0 ≤ i ≤ Z.of_nat (length keys))%Z ⌝)%I as "-#Hi".
-  { subst i. iPureIntro. split; [lia|apply Nat2Z.is_nonneg]. }
-  generalize i. clear i. intros i.
-  iLöb as "IH" forall (i). iDestruct "Hi" as %Hi_bounds.
-  destruct Hi_bounds as [Hi_nonneg Hi_upper].
-  destruct (decide (i < Z.of_nat (length keys))%Z).
-  2:{
-    assert (i = Z.of_nat (length keys)) by lia. subst.
-    rewrite Nat2Z.id drop_all /=.
-    wp_auto. wp_apply "Hown". iIntros "Hown".
-    wp_auto.
-    destruct Hfacts as (_ & Hkeys_len & _).
-    rewrite Hkeys_len.
-    iAssert (own_map_def mref dq m) with "[Hown]" as "Hm".
-    { iExists mv, mp. iFrame "Hown". iFrame "%". }
-    iApply ("HΦ" with "Hm HP").
-  }
-  list_elem keys i as key.
-  destruct (m !! key) eqn:Hlookup.
-  2:{
-    exfalso. apply not_elem_of_dom_2 in Hlookup as Hkey_m.
-    destruct Hfacts as [Hfact _]. rewrite <- Hfact in Hkey_m.
-    rewrite elem_of_list_to_set in Hkey_m.
-    rewrite list_elem_of_lookup in Hkey_m. Timeout 10 naive_solver.
-  }
-  iSpecialize ("Hiter" $! i key v with "[//] HP").
-  erewrite drop_S; last done. rewrite fmap_cons foldr_cons.
-  rewrite Hagree Hlookup /=. wp_apply (wp_wand with "Hiter").
-  iIntros (execv) "Hpost".
-  iDestruct "Hpost" as "[[-> H]|[-> H]]".
-  - wp_auto. rewrite continue_val_unseal. wp_auto.
-    replace (S (Z.to_nat i)) with (Z.to_nat (i + 1)) by lia.
-    iApply ("IH" with "[$Hown] H HΦ").
-    iPureIntro. lia.
-  - wp_auto. rewrite execute_val_unseal. wp_auto.
-    replace (S (Z.to_nat i)) with (Z.to_nat (i + 1)) by lia.
-    iApply ("IH" with "[$Hown] H HΦ").
-    iPureIntro. lia.
-Qed.
-
 Lemma wp_State__ListIdentities
     γ l resource dq attachments identity_policies q q_policy :
   {{{ "#Hiam" ∷ is_iam γ l ∗
@@ -314,36 +216,70 @@ Proof.
       "Hcap" ∷ own_slice_cap iammodel.IdentityID.t sl_current (DfracOwn 1) ∗
       "identity" ∷ identity_ptr ↦ identity_current ∗
       "%Hi_nonneg" ∷ ⌜ (0 ≤ i)%Z ⌝)%I)).
-  wp_apply (wp_map_for_range_return I with "[$Hinv_Hown_phys_identities]").
-  iIntros (keys) "%Hkeys".
+  iRename "Hinv_Hown_phys_identities" into "Hmap".
+  iCombineNamed "Hinv_*" as "Hinv_rest".
+  iCombine "Hinv_rest Hattachments Hidentity_policies Hown_Mutex HΦ"
+    as "Hrest".
+  iRename "Hmap" into "Hinv_Hown_phys_identities".
+  wp_bind (map.for_range _ _ _ _).
+  wp_call.
+  iDestruct (own_map_not_nil with "[$Hinv_Hown_phys_identities]") as %?.
+  wp_if_destruct; first by exfalso.
+  iEval (rewrite own_map_unseal) in "Hinv_Hown_phys_identities".
+  iNamed "Hinv_Hown_phys_identities".
+  wp_apply (_internal_wp_untyped_start_read with "Hown") as "Hown".
+  wp_apply (wp_InternalMapForRange with "[//]"). iIntros "%ks %Hdom'".
+  eapply go.is_map_domain_pure in Hdom'; last done.
+  destruct Hdom' as [Hks_nodup Hks].
+  assert (Forall (λ kv, ∃ (k : iammodel.IdentityID.t), kv = #k) ks) as Heq.
+  { rewrite Forall_forall. intros kv Hk.
+    specialize (Hks kv). specialize (Hdom kv).
+    rewrite -Hks in Hk. apply Hdom in Hk. done. }
+  apply Forall_exists_Forall2_l in Heq as [keys Heq].
+  apply Forall2_fmap_2 in Heq. rewrite -list_eq_Forall2 in Heq.
+  rewrite list_fmap_id in Heq. subst ks.
+  assert (Hkeys : list_to_set keys = dom phys_identities ∧
+                  length keys = size phys_identities ∧ NoDup keys).
+  { eassert _ as Hkeys_dom.
+    2:{ split; first eexact Hkeys_dom.
+        apply NoDup_fmap in Hks_nodup; last tc_solve.
+        rewrite <- (size_list_to_set (C:=gset iammodel.IdentityID.t));
+          last done.
+        rewrite Hkeys_dom. rewrite size_dom //. }
+    rewrite sets.set_eq. intros identity'.
+    rewrite elem_of_list_to_set.
+    specialize (Hks #identity'). specialize (Hagree identity').
+    rewrite list_elem_of_fmap_inj in Hks.
+    rewrite -Hks. rewrite Hagree. rewrite elem_of_dom.
+    by destruct lookup. }
   destruct Hkeys as (Hkeys_dom & Hkeys_length & Hkeys_nodup).
-  iSplitL "identities Hsl Hcap identity".
+  iAssert (I keys 0) with "[identities Hsl Hcap identity]" as "HI".
   { iExists sl, ""%go. iFrame. iPureIntro. word. }
-  iSplit.
-  - iModIntro.
-    iIntros (i identity policy_ids_l) "%Hiter HI".
-    destruct Hiter as [Hidentity_lookup Hphys_identity_lookup].
+  pose (i := 0%Z : Z).
+  change (I keys 0) with (I keys i).
+  replace (into_val <$> keys) with (into_val <$> (drop (Z.to_nat i) keys)).
+  2:{ rewrite drop_0 //. }
+  iAssert (⌜ (0 ≤ i ≤ Z.of_nat (length keys))%Z ⌝)%I as "-#Hi".
+  { subst i. iPureIntro. split; [lia|apply Nat2Z.is_nonneg]. }
+  generalize i. clear i. intros i.
+  iLöb as "IH" forall (i). iDestruct "Hi" as %Hi_bounds.
+  destruct Hi_bounds as [Hi_lower Hi_upper].
+  destruct (decide (i < Z.of_nat (length keys))%Z).
+  2:{
+    assert (i = Z.of_nat (length keys)) by lia. subst.
+    rewrite Nat2Z.id drop_all /=.
+    wp_auto. wp_apply "Hown". iIntros "Hown".
+    wp_auto.
+    iAssert (phys_identities_l ↦$ phys_identities) with "[Hown]"
+      as "Hinv_Hown_phys_identities".
+    { iEval (rewrite own_map_unseal). iExists mv, mp. iFrame "Hown".
+      iFrame "%". }
+    iDestruct "Hrest" as
+      "(Hinv_rest & Hattachments & Hidentity_policies & Hown_Mutex & HΦ)".
+    iNamed "Hinv_rest".
     iDestruct "HI" as (sl_current identity_current)
       "(identities & Hsl & Hcap & identity & %Hi_nonneg)".
-    wp_auto.
-    wp_apply wp_slice_literal. iSplitR; first done.
-    iIntros (sl0) "[Hsl0 _]". wp_auto.
-    wp_apply (wp_slice_append with "[$Hsl $Hcap $Hsl0]").
-    iIntros (sl') "(Hsl & Hcap & Hsl0)".
-    wp_auto.
-    iRight. iSplit; first done.
-    iExists sl', identity.
-    assert (take (Z.to_nat (i + 1)) keys =
-            take (Z.to_nat i) keys ++ [identity]) as Htake_succ.
-    { assert (Z.to_nat (i + 1) = S (Z.to_nat i)) as -> by lia.
-      apply take_S_r. done. }
-    rewrite Htake_succ.
-    iFrame.
-    iPureIntro. lia.
-  - iIntros "Hinv_Hown_phys_identities HI".
-    iDestruct "HI" as (sl_current identity_current)
-      "(identities & Hsl & Hcap & identity & %Hi_nonneg)".
-    rewrite Nat2Z.id -Hkeys_length.
+    rewrite Nat2Z.id.
     replace (take (length keys) keys) with keys by
       (symmetry; apply take_ge; done).
     wp_auto.
@@ -377,6 +313,39 @@ Proof.
     iApply "HΦ". iFrame.
     iSplit; first done.
     iSplit; done.
+  }
+  list_elem keys i as identity'.
+  destruct (phys_identities !! identity') eqn:Hlookup.
+  2:{
+    exfalso. apply not_elem_of_dom_2 in Hlookup as Hkey_m.
+    rewrite <- Hkeys_dom in Hkey_m.
+    rewrite elem_of_list_to_set in Hkey_m.
+    rewrite list_elem_of_lookup in Hkey_m. Timeout 10 naive_solver.
+  }
+  erewrite drop_S; last done. rewrite fmap_cons foldr_cons.
+  rewrite Hagree Hlookup /=.
+  iDestruct "HI" as (sl_current identity_current)
+    "(identities & Hsl & Hcap & identity & %Hi_nonneg)".
+  wp_auto.
+  wp_apply wp_slice_literal. iSplitR; first done.
+  iIntros (sl0) "[Hsl0 _]". wp_auto.
+  wp_apply (wp_slice_append with "[$Hsl $Hcap $Hsl0]").
+  iIntros (sl') "(Hsl & Hcap & Hsl0)".
+  wp_auto.
+  iAssert (I keys (i + 1)%Z) with
+    "[identities Hsl Hcap identity]" as "HI".
+  { iExists sl', identity'.
+    assert (take (Z.to_nat (i + 1)%Z) keys =
+            take (Z.to_nat i) keys ++ [identity']) as Htake_succ.
+    { assert (Z.to_nat (i + 1)%Z = S (Z.to_nat i)) as -> by lia.
+      apply take_S_r. done. }
+    rewrite Htake_succ.
+    iFrame.
+    iPureIntro. lia. }
+  rewrite execute_val_unseal. wp_auto.
+  replace (S (Z.to_nat i)) with (Z.to_nat (i + 1)%Z) by lia.
+  iApply ("IH" with "Hdefer Hrest Hown HI").
+  iPureIntro. lia.
 Unshelve.
 all: try done.
 Qed.
