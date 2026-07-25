54b1d2a63463345f371b37ceff787de9f78663ed
Xudong Sun <xudongs3@illinois.edu>
2026-07-25 15:45:44 +0800

    Prove wp_createPersistentVolumeClaim

--- src/proof/controllers/statefulset/create_pvc.v
+++ src/proof/controllers/statefulset/create_pvc.v
@@ -124,6 +124,121 @@ Lemma wp_createPersistentVolumeClaim γ model_l
           (new_persistent_volume_claim_key
             set claim_template ordinal)
   }}}.
-Proof. Admitted.
+Proof.
+  wp_start as "H". iNamed "H".
+  wp_auto.
+  wp_apply (wp_newPersistentVolumeClaim
+    set_l pod_l claim_template_l set pod claim_template ordinal
+    dq_set dq_pod dq_claim_template
+    with "[$Hset $Hpod $Hclaim_template]").
+  { iFrame "%". }
+  iIntros (claim_l) "H". iNamed "H".
+  iPoseProof (PersistentVolumeClaimV.deepown_l_split with "Hclaim") as
+    "(%Hclaim_l_not_null & Hclaim_typemeta & Hclaim_objectmeta_l &
+      Hclaim_spec_l & Hclaim_status_l)".
+  iDestruct "Hclaim_objectmeta_l" as (claim_meta_c)
+    "[Hclaim_objectmeta_field Hclaim_objectmeta]".
+  iNamedPrefix "Hclaim_objectmeta" "Hclaim_meta_".
+  wp_auto.
+  rewrite Hclaim_meta_Hdeepown_namespace Hclaim_meta_Hdeepown_name.
+  iAssert (is_pkg_init apimodel) as "#Hapimodel".
+  { iPkgInit. }
+  iDestruct "Hpvc_state" as "[Hpvc_exists | Hcreate]".
+  - iDestruct "Hpvc_exists" as (existing_claim) "H".
+    iNamed "H".
+    wp_apply (wp_State__PersistentVolumeClaimGet
+      γ model_l
+      (new_persistent_volume_claim_key set claim_template ordinal)
+      (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta').(ObjectMetaV.Namespace')
+      (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta').(ObjectMetaV.Name')
+      existing_claim.(PersistentVolumeClaimV.ObjectMeta').(ObjectMetaV.UID')
+      1
+      existing_claim.(PersistentVolumeClaimV.ObjectMeta')
+      existing_claim.(PersistentVolumeClaimV.Spec')
+      with "[$Hmeta $Hspec]").
+    { iFrame "#".
+      iPureIntro.
+      unfold new_persistent_volume_claim_key,
+        PersistentVolumeClaimV.key, PersistentVolumeClaimV.meta_key,
+        new_persistent_volume_claim.
+      done. }
+    iIntros (existing_claim_l existing_claim') "Hget".
+    iNamedPrefix "Hget" "Hget_".
+    wp_auto.
+    wp_apply (wp_IsNotFound interface.nil with "[]").
+    replace (bool_decide (not_found_error interface.nil)) with false by
+      (symmetry; apply bool_decide_false; exact not_found_error_nil).
+    wp_auto.
+    iApply "HΦ".
+    iFrame "Hset Hpod Hclaim_template".
+    iExists existing_claim.
+    iFrame.
+    iPureIntro.
+    exact Hkey.
+  - iNamed "Hcreate".
+    wp_apply (wp_State__PersistentVolumeClaimGet_reserved
+      γ model_l
+      (new_persistent_volume_claim_key set claim_template ordinal)
+      (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta').(ObjectMetaV.Namespace')
+      (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta').(ObjectMetaV.Name')
+      with "[$Hreserved]").
+    { iFrame "#".
+      iPureIntro.
+      unfold new_persistent_volume_claim_key,
+        PersistentVolumeClaimV.key, PersistentVolumeClaimV.meta_key,
+        new_persistent_volume_claim.
+      done. }
+    iIntros (err) "Hget". iNamed "Hget".
+    wp_auto.
+    wp_apply (wp_IsNotFound err with "[]").
+    replace (bool_decide (not_found_error err)) with true by
+      (symmetry; apply bool_decide_true; exact Hnot_found).
+    wp_auto.
+    rewrite Hclaim_meta_Hdeepown_namespace.
+    iCombineNamed "Hclaim_meta_*" as "Hclaim_objectmeta".
+    iAssert (ObjectMetaV.deepown claim_meta_c
+        (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta') 1)
+      with "[Hclaim_objectmeta]" as "Hclaim_objectmeta".
+    { iNamed "Hclaim_objectmeta".
+      rewrite /ObjectMetaV.deepown.
+      iFrame.
+      done. }
+    iAssert (ObjectMetaV.deepown_l
+        (PersistentVolumeClaimV.objectmeta_ptr claim_l)
+        (new_persistent_volume_claim set claim_template ordinal).(PersistentVolumeClaimV.ObjectMeta') 1)
+      with "[Hclaim_objectmeta_field Hclaim_objectmeta]"
+      as "Hclaim_objectmeta_l".
+    { iExists claim_meta_c.
+      iFrame "Hclaim_objectmeta_field".
+      iExact "Hclaim_objectmeta". }
+    iAssert (PersistentVolumeClaimV.deepown_l claim_l
+        (new_persistent_volume_claim set claim_template ordinal) 1)
+      with "[Hclaim_typemeta Hclaim_objectmeta_l
+        Hclaim_spec_l Hclaim_status_l]" as "Hclaim".
+    { iApply PersistentVolumeClaimV.deepown_l_restore;
+        [exact Hclaim_l_not_null | iFrame]. }
+    wp_apply (wp_State__PersistentVolumeClaimCreate_named_orphan
+      with "[$Hclaim $Hown_reserved_frag]").
+    { iFrame "# %".
+      iPureIntro.
+      split.
+      - unfold new_persistent_volume_claim_key,
+          PersistentVolumeClaimV.key, PersistentVolumeClaimV.meta_key.
+        done.
+      - unfold obj_parent_ref, meta_parent_ref,
+          new_persistent_volume_claim.
+        done. }
+    iIntros (created_claim_l created_claim uid) "Hcreated".
+    iNamed "Hcreated".
+    subst uid.
+    wp_auto.
+    iApply "HΦ".
+    iFrame "Hset Hpod Hclaim_template".
+    iExists created_claim.
+    iFrame.
+    iPureIntro.
+    symmetry.
+    exact Hkey_eq'.
+Qed.
 
 End proof.
